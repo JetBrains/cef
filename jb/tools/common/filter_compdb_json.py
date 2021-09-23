543752ceb74ef54d7ed5c7151c1c64e3192925b2
Anton Tarasov <anton.tarasov@jetbrains.com>
2021-09-23 17:57:46 +0300

    Fix cef/jb/tools/common/filter_compdb_json.py
    Now it counts lines after "file" and prints the result to stdout.

--- jb/tools/common/filter_compdb_json.py
+++ jb/tools/common/filter_compdb_json.py
@@ -1,8 +1,8 @@
-# Copyright 2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+# Copyright 2021 JetBrains s.r.o.
+# Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 import os
 import sys
-import ntpath
 
 
 def read_filters():
@@ -31,19 +31,13 @@ json_file_path = sys.argv[1]
 include_list_path = sys.argv[2]
 
 json_file_dir = os.path.dirname(os.path.abspath(json_file_path))
-out_filename = "filtered_" + ntpath.basename(json_file_path)
-out_filename_path = os.path.join(json_file_dir, out_filename)
 
 with open(json_file_path, 'r') as json_file_in, \
-        open(out_filename_path, 'w') as json_file_out, \
         open(include_list_path, 'r') as filter_file:
 
     filters = read_filters()
     compile_command = ""
-    first_write = True
-
-    json_file_out.truncate(0)
-    json_file_out.write("[\n")
+    print ("[")
 
     while True:
         line = json_file_in.readline()
@@ -52,16 +46,22 @@ with open(json_file_path, 'r') as json_file_in, \
         if not line or line == "]\n":
             break
 
-        compile_command += line
+        if "\"directory\":" in line:
+            compile_command += "    \"directory\": \"{}\",\n".format(json_file_dir)
+        else:
+            compile_command += line
+
         if "\"file\":" in line:
-            if apply_filter(line):
-                if not first_write:
-                    json_file_out.write("  },\n")
-                else:
-                    first_write = False
-                json_file_out.write(compile_command)
-            # skip "}"
-            json_file_in.readline()
+            fileLine = line
+
+            # add the lines after "file" including the closing "}"
+            while "}" not in line:
+                line = json_file_in.readline()
+                compile_command += line
+
+            if apply_filter(fileLine):
+                print (compile_command.rstrip())
+
             compile_command = ""
 
-    json_file_out.write("  }\n]\n")
+    print ("]")
