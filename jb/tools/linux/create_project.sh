a06a61a900463e598752ae05914df5371efc21c6
Anton Tarasov <anton.tarasov@jetbrains.com>
2021-11-16 15:06:37 +0300

    Add more jb/tools/linux scripts

--- jb/tools/linux/create_project.sh
+++ jb/tools/linux/create_project.sh
@@ -1,13 +1,26 @@
 # Copyright 2021 JetBrains s.r.o.
+# Creates cef project.
 
-[ -d ./chromium_git/chromium/src/cef ] || (echo "run jb/tools/common/get_sources.sh"; exit 1)
+root_dir=$(pwd)
+
+if [ ! -d "$root_dir"/chromium_git/chromium/src/cef ] ||
+   [ ! -d "$root_dir"/depot_tools ];
+then
+  echo "Error: please run jb/tools/common/get_sources.sh";
+  exit 1;
+fi
+export PATH="$root_dir"/depot_tools:$PATH
 
 apt-get --assume-yes update
 apt-get --assume-yes install lsb-core sudo python libgtkglext1-dev default-jdk vim
 
+echo "*** Installing build deps... ***"
 curl 'https://chromium.googlesource.com/chromium/src/+/master/build/install-build-deps.sh?format=TEXT' | base64 -d > install-build-deps.sh
-bash ./install-build-deps.sh --no-arm --no-chromeos-fonts --no-nacl
+bash "$root_dir"/install-build-deps.sh --no-arm --no-chromeos-fonts --no-nacl
 
-cd ./chromium_git/chromium/src/cef || exit 1
+cd "$root_dir"/chromium_git/chromium/src/cef || exit 1
+echo "*** Creating cef project... ***"
 export GN_DEFINES="is_official_build=true use_allocator=none is_cfi=false use_thin_lto=false use_sysroot=true no_crash_on_assert=true"
 bash cef_create_projects.sh
+
+cd "$root_dir" || exit 1
