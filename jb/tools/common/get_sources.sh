e55c75eb43b2a4ad01db4360b42e178dc6f96a19
Artem Bochkarev <artem.bochkarev@jetbrains.com>
2021-12-13 14:00:00 +0700

    [scripts] add possibility of clean chromium checkout
    also add separate script to checkout depot_tools

--- jb/tools/common/get_sources.sh
+++ jb/tools/common/get_sources.sh
@@ -4,16 +4,25 @@
 root_dir=$(pwd)
 cef_branch=${CEF_BRANCH:=jb_master}
 architecture=$1 # arm64 or x64
+clean=$2 # clean or clean-with-deps
 if [[ "${architecture}" != *arm64* ]]; then
   architecture="x64"
 fi
-
+cleankeys=""
+if [[ "${clean}" == "clean" ]]; then
+  echo "Will be performed clean checkout of Chromium"
+  cleankeys=" --force-clean"
+elif [[ "${clean}" == "clean-with-deps" ]]; then
+  echo "Will be performed clean checkout of Chromium with deps cleaning"
+  cleankeys=" --force-clean --force-clean-deps"
+fi
 
 if [ ! -d depot_tools ]; then
     echo "*** Clonning depot_tools... ***"
     git clone https://chromium.googlesource.com/chromium/tools/depot_tools
 fi
 cd depot_tools || exit 1
+git fetch
 git checkout main
 
 cd "$root_dir" || exit 1
@@ -24,24 +33,21 @@ fi
 cd "$root_dir"/cef || exit 1
 # needed for TeamCity
 git fetch https://github.com/JetBrains/cef master:origin/master
+echo "*** Checkout cef branch: ${cef_branch} ***"
 git checkout $cef_branch
 
 cd "$root_dir" || exit 1
 export PATH="$root_dir"/depot_tools:$PATH
 echo "*** Downloading chromium... ***"
-python "$root_dir"/cef/jb/tools/common/automate-git.py --download-dir="$root_dir"/chromium_git --depot-tools-dir="$root_dir"/depot_tools --branch=$cef_branch --no-depot-tools-update --no-distrib --no-build --${architecture}-build
+
+python "$root_dir"/cef/jb/tools/common/automate-git.py --download-dir="$root_dir"/chromium_git --depot-tools-dir="$root_dir"/depot_tools --branch=$cef_branch --no-depot-tools-update --no-distrib --no-build --${architecture}-build ${cleankeys}
 
 if [ $? -ne 0 ]; then
-	echo "*** Update sources failed. Checkout correct version of depot tools... ***"
-	cd "$root_dir"/chromium_git/chromium/src || exit 1
-	# shellcheck disable=SC2155
-	export COMMIT_DATE=$(git log -n 1 --pretty=format:%ci)
-	cd "$root_dir"/depot_tools || exit 1
-	git checkout "$(git rev-list -n 1 --before="$COMMIT_DATE" main)"
-	unset COMMIT_DATE
+	echo "*** Update sources failed... ***"
+	bash "$root_dir"/cef/jb/tools/common/checkout_depot_tools.sh
 
-  echo "*** Downloading chromium... ***"
-  python "$root_dir"/cef/jb/tools/common/automate-git.py --download-dir="$root_dir"/chromium_git --depot-tools-dir="$root_dir"/depot_tools --branch=$cef_branch --no-depot-tools-update --no-distrib --no-build --${architecture}-build
+  echo "*** Downloading chromium again... ***"
+  python "$root_dir"/cef/jb/tools/common/automate-git.py --download-dir="$root_dir"/chromium_git --depot-tools-dir="$root_dir"/depot_tools --branch=$cef_branch --no-depot-tools-update --no-distrib --no-build --${architecture}-build ${cleankeys}
 fi
 
 cd "$root_dir" || exit 1
