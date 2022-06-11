f00e1743b60dc48fe0e871ad1e2bd97e5257847d
Nikita Provotorov <nikita.provotorov@jetbrains.com>
2022-06-12 04:33:43 +0700

    fixup! build: JBR-4476 move linux build steps to a separate file

--- jb/tools/linux/teamcity_build.sh
+++ jb/tools/linux/teamcity_build.sh
@@ -2,6 +2,17 @@
 
 set -exuo pipefail
 
+architecture=$1 # arm64 or x64
+case "${architecture}" in
+    x64)
+      ;;
+    arm64)
+      ;;
+    *)
+      echo "Unsupported arch: ${architecture}"
+      exit 1
+esac
+
 # CD to the directory where cef/jb/tools/linux is a valid path
 cd "$(dirname "$0")/../../../.."
 
@@ -9,10 +20,10 @@ echo "Build env:"
 cat cef/jb/tools/linux/build.env
 
 echo "Create project:"
-bash cef/jb/tools/linux/create_project.sh x64
+bash cef/jb/tools/linux/create_project.sh "${architecture}"
 
 echo "Build project:"
-bash cef/jb/tools/linux/build.sh x64
+bash cef/jb/tools/linux/build.sh "${architecture}"
 
 echo "Create distributive:"
-bash cef/jb/tools/linux/create_distr.sh x64
+bash cef/jb/tools/linux/create_distr.sh "${architecture}"
